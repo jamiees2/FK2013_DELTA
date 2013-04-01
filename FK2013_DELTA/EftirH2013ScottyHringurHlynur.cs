3a4cb9152c8dfca4269d14d5eff0178d463ee55b
Hlynur Óskar Guðmundsson <runylh@gmail.com>
2013-04-01 19:01:01 +0000

    Hlynur's soloutions

---
+++ FK2013_DELTA/EftirH2013ScottyHringurHlynur.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using Pie; //https://github.com/jamiees2/Pie
+using Pie.Strings;
+using Pie.Math;
+using Pie.Math.Geometry;
+using Pie.Time;
+using Pie.Units;
+using Pie.IO;
+using Pie.FK;
+using Pie.CharShapes;
+using Console = Pie.PieConsole;
+
+namespace Forritunarkeppnin_2013_Scotty
+{
+    class FK2013FH : FKController
+    {
+        public FK2013FH(string title = null, string[] args = null) : base(title, args) { }
+
+        [FKAction("1", "Problem 1")]
+        public void Problem1()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        [FKAction("2", "Problem 2")]
+        public void Problem2()
+        {
+            double d;
+            Console.ReadLineQ<double>(out d, "Kommutala").WriteLine(Math.Round(d, MidpointRounding.AwayFromZero));
+        }
+
+        [FKAction("3", "Problem 3")]
+        public void Problem3()
+        {
+            int n;
+            Console.ReadLineQ<int>(out n, "Tala");
+            for (int i = n; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("BOOM!!!");
+        }
+
+        [FKAction("5", "Problem 5")]
+        public void Problem5()
+        {
+            string s;
+            Console.ReadLineQ(out s, "Strengur").WriteLine("Thad eru {0} stafir i strengnum", s.Count(Char.IsLetter));//Not count integers
+        }
+
+        [FKAction("7", "Problem 7")]
+        public void Problem7()
+        {
+            string nafn, aldur, kyn;
+            Console.ReadLineQ(out nafn, "Nafn").ReadLineQ(out aldur, "Aldur").ReadLineQ(out kyn, "Kyn");
+            if (kyn == "kk")
+                kyn = "Hann";
+            else if (kyn == "kvk")
+                kyn = "Hún";
+            Console.WriteLine(kyn + " á afmæli í dag,\n" + kyn + " á afmæli í dag.\n" + kyn + " á afmæli " + kyn + " " + nafn + ",\n" + kyn + " á afmæli í dag.\n\n" + kyn + " er " + aldur + " ára í dag,\n" + kyn + " er " + aldur + " ára í dag.\n" + kyn + " er " + aldur + " ára " + kyn + " " + nafn + ",\n" + kyn + " er " + aldur + " ára í dag.");
+        }
+
+        [FKAction("8", "Problem 8")]
+        public void Problem8()
+        {
+            int f, s;
+            Console.ReadLineQ<int>(out f, "Fyrri heiltala").ReadLineQ<int>(out s, "Seinni heiltala").WriteLine(s + " gengur {1} sinnum upp í {0} \n{2}", f, Math.DivRem(f, s, out f), (f != 0 ? "Afgangur er " + f : "Enginn afgangur"));
+        }
+
+        [FKAction("9", "Problem 9")]
+        public void Problem9()
+        {
+            int f, s;
+            string opt = "";
+            while (opt != "n")
+            {
+                Console.ReadLineQ<int>(out f, "Fyrri heiltala").ReadLineQ<int>(out s, "Seinni heiltala").WriteLine(s + " gengur {1} sinnum upp í {0} \n{2}", f, Math.DivRem(f, s, out f), (f != 0 ? "Afgangur er " + f : "Enginn afgangur"));
+                Console.ReadLineQ(out opt, "Viltu hætta(j/n)");
+            }
+        }
+
+        [FKAction("13", "Problem 13")]
+        public void Problem13()
+        {
+            int n;
+
+            Console.ReadLineQ<int>(out n, "Stærð");
+            if (n > 0 && n < 20 && n % 2 != 0)
+            {
+                int[][] spiral = new int[n][];
+                for (int i = 0; i < spiral.Length; i++)
+                {
+                    spiral[i] = new int[n];
+                }
+                getSpiral(ref spiral);
+                for (int i = 0; i < spiral.Length; i++)
+                {
+                    for (int j = 0; j < spiral[i].Length; j++)
+                    {
+                        Console.Write(spiral[i][j].ToString().PadLeft(3) + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+                Console.WriteLine("Ólögleg stærð");
+        }
+
+        public static void getSpiral(ref int[][] input)
+        {
+            int x = input.Length - 1;
+            int y = input[0].Length - 1;
+            getSpiral(input, x, y, (x + 1) * (y + 1) - 1, 0);
+        }
+
+        public static void getSpiral(int[][] input, int x, int y, int n, int a)
+        {
+            input[x][y] = n--;
+
+            if (a < 1 && y > 0 && input[x][y - 1] < n) // left
+                getSpiral(input, x, y - 1, n, 0);
+            else if (a < 2 && x > 0 && input[x - 1][y] < n) // top
+                getSpiral(input, x - 1, y, n, 1);
+            else if (a < 3 && (y + 1) < input[0].Length && input[x][y + 1] < n) // right
+                getSpiral(input, x, y + 1, n, 2);
+            else if (a < 4 && (x + 1) < input.Length && input[x + 1][y] < n) // down
+                getSpiral(input, x + 1, y, n, 3);
+            else if (y > 0 && input[x][y - 1] < n)// left
+                getSpiral(input, x, y - 1, n, 0);
+
+            return;
+        }
+
+        [FKAction("16", "Problem 16")]
+        public void Problem16()
+        {
+            string[] Null = new string[] { "@@@ ", "@ @ ", "@ @ ", "@ @ ", "@@@ " };
+            string[] einn = new string[] { "  @ ", "  @ ", "  @ ", "  @ ", "  @ " };
+            string[] tveir = new string[] { "@@@ ", "  @ ", "@@@ ", "@   ", "@@@ " };
+            string[] thrir = new string[] { "@@@ ", "  @ ", "@@@ ", "  @ ", "@@@ " };
+            string[] fjorir = new string[] { "@ @ ", "@ @ ", "@@@ ", "  @ ", "  @ " };
+            string[] fimm = new string[] { "@@@ ", "@   ", "@@@ ", "@   ", "@@@ " };
+            string[] sex = new string[] { "@@@ ", "@ @ ", "@ @ ", "@ @ ", "@@@ " };
+            string[] sjo = new string[] { "@@@ ", "  @ ", "  @ ", "  @ ", "  @ " };
+            string[] atta = new string[] { "@@@ ", "@ @ ", "@@@ ", "@ @ ", "@@@ " };
+            string[] niu = new string[] { "@@@ ", "@ @ ", "@@@ ", "  @ ", "  @ " };
+            string[][] s = new[] { Null, einn, tveir, thrir, fjorir, fimm, sex, sjo, atta, niu };
+            int n;
+            Console.ReadLineQ<int>(out n, "Numer");
+            for (int i = 0; i < 5; i++)
+            {
+                foreach (int k in n.Digits())
+                {
+                    Console.Write(s[k][i]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ FK2013_DELTA/FyrirH2013ScottyHringurHlynur.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using Pie; //https://github.com/jamiees2/Pie
+using Pie.Strings;
+using Pie.Math;
+using Pie.Math.Geometry;
+using Pie.Time;
+using Pie.Units;
+using Pie.IO;
+using Pie.FK;
+using Pie.CharShapes;
+using Console = Pie.PieConsole;
+
+namespace Forritunarkeppnin_2013_Scotty
+{
+    class FK2013 : FKController
+    {
+        public FK2013(string title = null, string[] args = null) : base(title, args) { }
+
+        [FKAction("1", "Problem 1")]
+        public void Problem1()
+        {
+            int n;
+            Console.ReadLineQ<int>(out n, "N");
+            for (int i = 0; i < n+1; i++)
+            {
+                Console.WriteLine("2 ^ "+i+" = "+ Math.Pow(2,i));
+            }
+        }
+
+        [FKAction("2", "Problem 2")]
+        public void Problem2()
+        {
+            string s;
+            
+            Console.ReadLineQ(out s, "Setning");
+            string[] sl = s.Split();
+            for (int i = 0; i < sl.Length; i++)
+            {
+                Console.Write(sl[i].Reverse()+" ");
+            }
+        }
+
+        [FKAction("3", "Problem 3")]
+        public void Problem3()
+        {
+            double w, l, h;
+            Console.ReadLineQ<double>(out w, "Breidd").ReadLineQ<double>(out l, "Lengd").ReadLineQ<double>(out h, "Hæð").WriteLine("Stærsta r = {0}", Math.Min(w,Math.Min(l,h))/2);
+        }
+
+        [FKAction("5", "Problem 5")]
+        public void Problem5()
+        {
+            int opt=-1;
+            while (opt!=3)
+            {
+                Console.ReadLineQ(out opt, "1. Breyta Kelvin í Fahrenheit\n2. Breyta Fahrenheit í Kelvin\n3. Hætta\nVal");
+                switch (opt)
+                {
+                    case 1:
+                        double k;
+                        Console.ReadLineQ<double>(out k, "Kelvin").WriteLine((Fahrenheit)((Kelvin)k));
+                        break;
+                    case 2:
+                        double f;
+                        Console.ReadLineQ<double>(out f, "Fahrenheit").WriteLine((Kelvin)((Fahrenheit)f));
+                        break;
+                    default:
+                        break;
+                } 
+            }
+        }
+
+        [FKAction("7", "Problem 7")]
+        public void Problem7()
+        {
+            int f;
+            Console.ReadLineQ<int>(out f, "Fjoldi lina");
+            List<string> a = Console.ReadLines().Take(f).ToList();
+            for (int i = 0; i < a.Select(x => x.Length).Max(); i++)
+            {
+                for (int n = 0; n < a.Count; n++)
+                {
+                    if(a[n].Length>i)
+                        Console.Write(a[n][i]+" ");
+                    else
+                    Console.Write("  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        [FKAction("10", "Problem 10")]
+        public void Problem10()
+        {
+            int m, md;
+            Console.ReadLineQ<int>(out m, "Dagar í mánuðinum").ReadLineQ<int>(out md, "Fyrsti dagur mánaðarins").WriteLine("Sun Mán Þri Mið Fim Fös Lau");
+            Console.Write(1.ToString().PadLeft(4 * md-1));
+            if (md == 7)
+            {
+                Console.WriteLine();
+            }
+            int iA = md+1;
+            for (int i = 2; i <= m; i++)
+            {
+                if (iA % 7 == 0)
+                    Console.WriteLine(i.ToString().PadLeft(4));
+                else
+                    Console.Write(i.ToString().PadLeft(4));
+                iA++;
+            }
+        }
+
+        [FKAction("14", "Problem 14")]
+        public void Problem14()
+        {
+            int n;
+            Console.ReadLineQ<int>(out n, "n");
+            if (n > 0 && n < Math.Pow(10, 8))
+            {
+                if (n.ProperDivisors().Sum() == n)
+                    Console.WriteLine("Fullkomin");
+                else
+                    Console.WriteLine("Ekki fullkomin");
+            }
+            else
+                Console.WriteLine("Vitlaus innsláttur");
+        }
+
+        [FKAction("16", "Problem 16")]
+        public void Problem16()
+        {
+            Dictionary<string, double> d = new Dictionary<string,double>();
+            int f;
+            Console.ReadLineQ<int>(out f, "Fjöldi");
+            for (int i = 0; i < f; i++)
+            {
+                string n;
+                double h, t;
+                Console.ReadLineQ(out n, "Nafn " + (i + 1)).ReadLineQ<double>(out h, "Hæð " + (i + 1)).ReadLineQ<double>(out t, "Þyngd " + (i + 1));
+                Console.WriteLine("\n");
+                d.Add(n, t / Math.Pow(h, 2));
+            }
+            foreach (var i in d.OrderBy(x => x.Value))
+            {
+                Console.WriteLine("{0} {1:0.#}" ,i.Key, i.Value);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new FK2013FH("FK2013FH", args).Run();
+            //(new FK2013("FK2013", args)).Run();
+        }
+    }
+}
